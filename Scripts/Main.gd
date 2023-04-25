a10c143c0ed151172af2d05430b0b6fcdb8ac838
stran96r <stran96r@gmail.com>
2023-04-26 04:02:58 +0430

    look at me func added for gun

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -6,6 +6,9 @@ var ray_target = Vector3()
 @onready
 var player = $Player
 
+@onready
+var players_hand = $Player/Body/Hand
+
 func _physics_process(delta):
 	if is_instance_valid(player):
 		var mouse_position = get_viewport().get_mouse_position()	
@@ -21,3 +24,6 @@ func _physics_process(delta):
 			var position = intersection.values()[0]
 			var look_at_me = Vector3(position.x, player.position.y, position.z)
 			player.look_at(look_at_me)
+			var distance_to_pointer = players_hand.global_transform.origin - look_at_me
+			if distance_to_pointer.length() > 3:
+				players_hand.look_at(look_at_me, Vector3.UP)
