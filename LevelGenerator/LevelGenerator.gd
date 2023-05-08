4d5cd9f073832179e4b92d04497497810eecccbc
stran96r <stran96r@gmail.com>
2023-05-08 05:09:28 +0430

    floof fill alg successfully implemented

--- LevelGenerator/LevelGenerator.gd
+++ LevelGenerator/LevelGenerator.gd
@@ -12,6 +12,7 @@ var ObstacleScene: PackedScene
 var map_width = 11 : 
 	set(value):
 		map_width = make_odd(value, map_width)
+		update_map_center()
 		generate_map()
 	get:
 		return map_width
@@ -20,6 +21,7 @@ var map_width = 11 :
 var map_depth = 11 : 
 	set(value):
 		map_depth = make_odd(value, map_depth)
+		update_map_center()
 		generate_map()
 	get:
 		return map_depth
@@ -72,8 +74,6 @@ var obstacle_min_height = 1:
 	get:
 		return obstacle_min_height		
 
-var map_coords_array : Array = []
-
 class Coord:
 	var x: int
 	var z: int
@@ -85,8 +85,20 @@ class Coord:
 	func _to_string():
 		return "(" + str(x) + "," + str(z) + ")"
 	
+	func check_for_equals(coord):
+		return self.x == coord.x and self.z == coord.z
+	
+var map_coords_array : Array = []
+var obstacle_map : Array = []
+var map_center := Coord.new(map_width/2, map_depth/2)
+
+
 func _ready():
+	update_map_center()
 	generate_map()
+
+func update_map_center():
+	map_center = Coord.new(map_width/2, map_depth/2)
 	
 func make_odd(new_int, old_int):
 	if new_int % 2 == 0:
@@ -102,6 +114,13 @@ func fill_map_coords_array():
 	for x in range(map_width):
 		for z in range(map_depth):
 			map_coords_array.append(Coord.new(x, z))	
+
+func fill_obstacle_map():
+	obstacle_map = []
+	for x in range(map_width):
+		obstacle_map.append([])
+		for z in range(map_depth):
+			obstacle_map[x].append(false)
 
 func generate_map():
 	print("Generating the map...")
@@ -125,20 +144,60 @@ func add_ground():
 
 func add_obstacles():
 	fill_map_coords_array()
-#	print(map_coords_array)
+	fill_obstacle_map()
+	
 	seed(rng_seed)
 	map_coords_array.shuffle()
-#	print(map_coords_array)
-	print("Generating the obstacles...")
 	
 	var number_of_obstacles: int = map_coords_array.size() * obstacle_density
+	var current_obstacle_count = 0
 	if number_of_obstacles > 0:
 		for coord in map_coords_array.slice(0, number_of_obstacles):
-			create_obstacles_at(coord.x, coord.z)
-#	for x in range(map_width):
-#		for z in range(map_depth):
-#			if randf() < obstacle_density:
+			if !map_center.check_for_equals(coord):
+				
+				current_obstacle_count += 1
+				obstacle_map[coord.x][coord.z] = true
+				if map_is_fully_accessable(current_obstacle_count):
+					create_obstacles_at(coord.x, coord.z)
+				else:
+					current_obstacle_count -= 1
+					obstacle_map[coord.x][coord.z] = false
 
+func map_is_fully_accessable(current_obstacle_count):
+	var we_already_checked_here = []
+	for x in range(map_width):
+		we_already_checked_here.append([])
+		for z in range(map_depth):
+			we_already_checked_here[x].append(false)
+	
+	var coords_to_check = [map_center]
+	we_already_checked_here[map_center.x][map_center.z] = true
+	var accessable_tile_count = 1
+	
+	while coords_to_check:
+		var corrent_tile: Coord = coords_to_check.pop_front()
+		for x in [-1, 0, 1]:
+			for z in [-1, 0, 1]:
+				if x == 0 or z == 0:
+					var neighbor = Coord.new(corrent_tile.x + x, corrent_tile.z + z)
+					if on_the_map(neighbor):
+						if not we_already_checked_here[neighbor.x][neighbor.z]:
+							if not obstacle_map[neighbor.x][neighbor.z]:
+								we_already_checked_here[neighbor.x][neighbor.z] = true
+								coords_to_check.append(neighbor)
+								accessable_tile_count += 1
+								
+	var target_accessable_tile_count = (map_depth * map_width) - current_obstacle_count
+	
+	return true if target_accessable_tile_count == accessable_tile_count else false
+#	if target_accessable_tile_count == accessable_tile_count:
+#		return true
+#	else:
+#		return false
+	
+func on_the_map(neighbor):
+	return neighbor.x >= 0 and neighbor.x < map_width and neighbor.z >= 0 and neighbor.z < map_depth	
+		
 func create_obstacles_at(x,z):
 	var temp
 	var obstacle_position = Vector3(x * 2 - map_width + 1, 0, z * 2 - map_depth + 1)
